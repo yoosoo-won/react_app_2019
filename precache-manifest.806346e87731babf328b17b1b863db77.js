a258fdc2a2d492ded3a496ab6b0d1a5528cbf3a1
Yoosoo Won <Sihyun@Yoosooui-MacBook-Pro.local>
2019-10-13 16:45:14 +0900

    Updates

---
+++ precache-manifest.806346e87731babf328b17b1b863db77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3988b705d4bc61ce9b6a0b5f19b96f9",
+    "url": "/react_app_2019/index.html"
+  },
+  {
+    "revision": "b756c8d10e420dc25462",
+    "url": "/react_app_2019/static/css/main.9b093228.chunk.css"
+  },
+  {
+    "revision": "d6e8c4ce4ee51ea18bef",
+    "url": "/react_app_2019/static/js/2.ebf0c610.chunk.js"
+  },
+  {
+    "revision": "b756c8d10e420dc25462",
+    "url": "/react_app_2019/static/js/main.b8d81284.chunk.js"
+  },
+  {
+    "revision": "738152f96a1557530c14",
+    "url": "/react_app_2019/static/js/runtime-main.df189661.js"
+  }
+]);
